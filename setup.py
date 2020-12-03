f26268fa29057596d47a70eedc0406eb567c3e35
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-12-02 20:08:32 -0800

    create package, setup.py and testing framework for better development flows

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages  # , Extension
+
+
+setup(name="rrna_scripts",
+      version="0.0.1",
+      description="Scripts and helper functions for data analysis of yeast rrna signalAlign sequencing data",
+      author="Andrew Bailey",
+      author_email="andbaile@ucsc.edu",
+      url="https://github.com/adbailey4/rrna_scripts",
+      package_dir={"": "src"},
+      # ext_modules=extensions,
+      packages=find_packages("src"),
+      scripts=["src/rrna_analysis/scripts/re_run_signalalign.py",
+               "src/rrna_analysis/scripts/plot_per_position_kmer_distributions.py",
+               "src/rrna_analysis/scripts/run_embed_plot_wrapper.py",
+               "src/rrna_analysis/scripts/train_test_accuracy_wrapper.py"],
+      install_requires=["numpy>=1.9.2",
+                        "pysam>=0.8.2.1,<0.16.0",
+                        "pandas>=0.23.1",
+                        "matplotlib>=2.0.2",
+                        "signalAlign>=0.3.0",
+                        "py3helpers[seq_tools]>=0.5.0",
+                        'embed>=0.0.5']
+      )
---
+++ tests/test_multiple_model_accuracy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Tests for multiple_model_accuracy.py"""
+########################################################################
+# File: multiple_model_accuracy.py
+#  executable: multiple_model_accuracy.py
+#
+# Author: Andrew Bailey
+# History: Created 12/02/20
+########################################################################
+import unittest
+import os
+
+# from py3helpers.utils import merge_dicts, captured_output
+# from signalalign.alignedsignal import *
+# from signalalign.signalAlignment import SignalAlignment, create_signalAlignment_args
+
+
+class MultipleModelAccuracy(unittest.TestCase):
+
+    tmp_directory = None
+
+    @classmethod
+    def setUpClass(cls):
+        super(MultipleModelAccuracy, cls).setUpClass()
+        cls.HOME = '/'.join(os.path.abspath(__file__).split("/")[:-4])
+        cls.test_file = os.path.join(cls.HOME, "src/rrna_analysis/__init__.py")
+
+    def test_test(self):
+        self.assertTrue(os.path.exists(self.test_file))
+
+
+if __name__ == "__main__":
+    unittest.main()
